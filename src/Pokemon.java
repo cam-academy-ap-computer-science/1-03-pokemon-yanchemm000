983fafa9a816da58788141e827a4a3b66bed2214
yanchemm000 <yanchemm000@CAM-LAB-29.bgsd.k12.wa.us>
2019-09-09 07:26:30 -0700

    Finished Pokemon at School

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -3,7 +3,10 @@ public class Pokemon {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Pikachu welcomes you to the world of Pokemon");
+		System.out.println(" (\\__/)");
+		System.out.println(" (o^-^)");
+		System.out.println("z(_(\")(\")");
 	}
 
 }
